41682e9f93baaa0c45493b5018b9e6aaf44d88e5
acple <silentsphere110@gmail.com>
2015-12-24 22:55:30 +0900

    Bindに直接対応していたSelectManyを削除

--- ParsecSharp/Parser/Parser.Linq.cs
+++ ParsecSharp/Parser/Parser.Linq.cs
@@ -7,9 +7,6 @@ namespace Parsec
         public static Parser<TToken, TResult> Select<TToken, T, TResult>(this Parser<TToken, T> parser, Func<T, TResult> selector)
             => parser.FMap(selector);
 
-        public static Parser<TToken, TResult> SelectMany<TToken, T, TResult>(this Parser<TToken, T> parser, Func<T, Parser<TToken, TResult>> selector)
-            => parser.Bind(selector);
-
         public static Parser<TToken, TResult> SelectMany<TToken, T, TTemp, TResult>(this Parser<TToken, T> parser, Func<T, Parser<TToken, TTemp>> selector, Func<T, TTemp, TResult> resultSelector)
             => parser.Bind(x => selector(x).FMap(y => resultSelector(x, y)));
 
