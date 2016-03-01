d30f576ceb7f2d75e0e912ad379cb316f0eb1075
acple <silentsphere110@gmail.com>
2016-03-02 08:42:00 +0900

    UnitにIEquatableを実装

--- ParsecSharp/Core/Unit.cs
+++ ParsecSharp/Core/Unit.cs
@@ -1,11 +1,16 @@
-﻿namespace Parsec
+﻿using System;
+
+namespace Parsec
 {
-    public sealed class Unit
+    public sealed class Unit : IEquatable<Unit>
     {
         public static Unit Instance { get; } = new Unit();
 
         private Unit()
         { }
+
+        public bool Equals(Unit other)
+            => other != null;
 
         public override bool Equals(object obj)
             => obj is Unit;
