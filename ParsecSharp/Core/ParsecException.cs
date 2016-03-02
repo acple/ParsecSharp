5721e6dc0b3e59df5882330056c369e530182670
acple <silentsphere110@gmail.com>
2016-03-02 14:34:24 +0900

    ParsecExceptionのコンストラクタを公開

--- ParsecSharp/Core/ParsecException.cs
+++ ParsecSharp/Core/ParsecException.cs
@@ -6,12 +6,12 @@ namespace Parsec
     {
         public IParsecState<T> State { get; }
 
-        internal ParsecException(string message, IParsecState<T> state) : base(message)
+        public ParsecException(string message, IParsecState<T> state) : base(message)
         {
             this.State = state;
         }
 
-        internal ParsecException(string message, Exception exception, IParsecState<T> state) : base(message, exception)
+        public ParsecException(string message, Exception exception, IParsecState<T> state) : base(message, exception)
         {
             this.State = state;
         }
