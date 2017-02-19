ea36f88d4a9f9f1ed467bf03bfdad823703184e9
acple <silentsphere110@gmail.com>
2017-02-19 16:19:05 +0900

    表現を微修正

--- ParsecSharp/Parser/Parser.OtherExtensions.cs
+++ ParsecSharp/Parser/Parser.OtherExtensions.cs
@@ -6,9 +6,9 @@ namespace Parsec
     public static partial class Parser
     {
         public static void CaseOf<TToken, T>(this Result<TToken, T> result, Action<Fail<TToken, T>> fail, Action<Success<TToken, T>> success)
-            => result.CaseOf<object>(
-                x => { fail(x); return null; },
-                x => { success(x); return null; });
+            => result.CaseOf(
+                x => { fail(x); return default(object); },
+                x => { success(x); return default(object); });
 
         public static Parser<TToken, T> Do<TToken, T>(this Parser<TToken, T> parser, Action<T> action)
             => parser.FMap(x => { action(x); return x; });
