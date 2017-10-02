17e897fcc21f1d6507f1d5096f78eefab0c3c795
acple <silentsphere110@gmail.com>
2017-10-03 00:03:52 +0900

    IEnumerable<string>に対するToStr()メソッドを追加

--- ParsecSharp/Parser/Text.Extensions.cs
+++ ParsecSharp/Parser/Text.Extensions.cs
@@ -18,5 +18,8 @@ namespace Parsec
 
         public static Parser<char, string> ToStr(this Parser<char, IEnumerable<char>> parser)
             => parser.Map(x => new string(x.ToArray()));
+
+        public static Parser<char, string> ToStr(this Parser<char, IEnumerable<string>> parser)
+            => parser.Map(x => string.Join(string.Empty, x));
     }
 }
