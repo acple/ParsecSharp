3c7518f65553cbb441265fbe8f652931d6c5b826
acple <silentsphere110@gmail.com>
2017-12-04 22:08:26 +0900

    どうでもいい変更

--- ParsecSharp/Parser/Internal/Utility.cs
+++ ParsecSharp/Parser/Internal/Utility.cs
@@ -3,7 +3,7 @@
     internal static partial class Utility
     {
         public static IParsecStateStream<TToken> Advance<TToken>(this IParsecStateStream<TToken> state, int count)
-            => (1 <= count && state.HasValue)
+            => (0 < count && state.HasValue)
                 ? state.Next.Advance(count - 1)
                 : state;
     }
