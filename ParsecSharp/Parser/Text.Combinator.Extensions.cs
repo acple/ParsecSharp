1f25a4f99548b654ffa4abcafcf7ffe3d21210a7
acple <silentsphere110@gmail.com>
2018-03-13 16:06:07 +0900

    利便性のためToIntを追加

--- ParsecSharp/Parser/Text.Combinator.Extensions.cs
+++ ParsecSharp/Parser/Text.Combinator.Extensions.cs
@@ -8,6 +8,9 @@ namespace Parsec
         public static Parser<char, string> ToStr(this Parser<char, IEnumerable<char>> parser)
             => parser.Map(x => new string(x.ToArray()));
 
+        public static Parser<char, int> ToInt(this Parser<char, string> parser)
+            => parser.Bind(number => (int.TryParse(number, out var integer)) ? Pure(integer) : Fail<int>());
+
         public static Parser<char, string> Join(this Parser<char, IEnumerable<string>> parser)
             => parser.Join(string.Empty);
 
