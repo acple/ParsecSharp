f19e13042d77bdb536b0de549f2b1e9d7578e4f5
acple <silentsphere110@gmail.com>
2018-04-11 02:48:10 +0900

    OneOfとNoneOfにparamsを取るオーバーロードを追加

--- ParsecSharp/Parser/Parser.Prim.cs
+++ ParsecSharp/Parser/Parser.Prim.cs
@@ -16,8 +16,14 @@ namespace Parsec
         public static Parser<TToken, TToken> OneOf<TToken>(IEnumerable<TToken> candidates)
             => Satisfy<TToken>(x => candidates.Contains(x));
 
+        public static Parser<TToken, TToken> OneOf<TToken>(params TToken[] candidates)
+            => OneOf(candidates.AsEnumerable());
+
         public static Parser<TToken, TToken> NoneOf<TToken>(IEnumerable<TToken> candidates)
             => Satisfy<TToken>(x => !candidates.Contains(x));
+
+        public static Parser<TToken, TToken> NoneOf<TToken>(params TToken[] candidates)
+            => NoneOf(candidates.AsEnumerable());
 
         public static Parser<TToken, TToken> Satisfy<TToken>(Func<TToken, bool> predicate)
             => Builder.Create<TToken, TToken>(state => (state.HasValue && predicate(state.Current))
