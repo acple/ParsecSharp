0473a1aaa5cb9e52df2bf92db310e55fb708a114
acple <silentsphere110@gmail.com>
2019-01-29 10:37:42 +0900

    remove index field from ArrayStream

--- ParsecSharp/Data/ArrayStream.cs
+++ ParsecSharp/Data/ArrayStream.cs
@@ -7,25 +7,22 @@ namespace ParsecSharp
     {
         private readonly IReadOnlyList<TToken> _source;
 
-        private readonly int _index;
-
         private readonly LinearPosition _position;
 
-        public TToken Current => this._source[this._index];
+        public TToken Current => this._source[this._position.Column];
 
-        public bool HasValue => this._index < this._source.Count;
+        public bool HasValue => this._position.Column < this._source.Count;
 
         public IPosition Position => this._position;
 
-        public IParsecStateStream<TToken> Next => new ArrayStream<TToken>(this._source, this._index + 1, this._position.Next());
+        public IParsecStateStream<TToken> Next => new ArrayStream<TToken>(this._source, this._position.Next());
 
-        public ArrayStream(IReadOnlyList<TToken> source) : this(source, 0, LinearPosition.Initial)
+        public ArrayStream(IReadOnlyList<TToken> source) : this(source, LinearPosition.Initial)
         { }
 
-        private ArrayStream(IReadOnlyList<TToken> source, int index, LinearPosition position)
+        private ArrayStream(IReadOnlyList<TToken> source, LinearPosition position)
         {
             this._source = source;
-            this._index = index;
             this._position = position;
         }
 
