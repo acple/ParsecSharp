5cb6be2386b89f6d11859f159e337ab92cb2c974
acple <silentsphere110@gmail.com>
2019-03-22 14:11:51 +0900

    add DecDigit

--- ParsecSharp/Parser/Text.Prim.cs
+++ ParsecSharp/Parser/Text.Prim.cs
@@ -78,6 +78,10 @@ namespace ParsecSharp
             => OneOf("01234567");
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Parser<char, char> DecDigit()
+            => OneOf("0123456789");
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Parser<char, char> HexDigit()
             => OneOf("0123456789ABCDEFabcdef");
 
