d31b37a9fdcda5a34fcd98be0377e571bf31ef54
acple <silentsphere110@gmail.com>
2019-05-31 12:41:57 +0900

    rewrite EnumerableStream to use buffering

--- ParsecSharp/Data/EnumerableStream.cs
+++ ParsecSharp/Data/EnumerableStream.cs
@@ -1,57 +1,86 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ParsecSharp.Internal;
 
 namespace ParsecSharp
 {
     public sealed class EnumerableStream<TToken> : IParsecStateStream<TToken>
     {
-        private readonly IDisposable disposable;
+        private const int MaxBufferSize = 1024;
+
+        private sealed class Buffer : IDisposable
+        {
+            private readonly IDisposable disposable;
+
+            private readonly Lazy<Buffer> _next;
+
+            public TToken[] Current { get; }
+
+            public Buffer Next => this._next.Value;
+
+            public Buffer(IEnumerator<TToken> enumerator)
+            {
+                this.disposable = enumerator;
+                try
+                {
+                    this.Current = Enumerable.Repeat(enumerator, MaxBufferSize)
+                        .TakeWhile(enumerator => enumerator.MoveNext())
+                        .Select(enumerator => enumerator.Current)
+                        .ToArray();
+                }
+                catch
+                {
+                    this.Dispose();
+                    throw;
+                }
+                finally
+                {
+                    this._next = new Lazy<Buffer>(() => new Buffer(enumerator));
+                }
+            }
+
+            public void Dispose()
+                => this.disposable.Dispose();
+        }
+
+        private readonly Buffer buffer;
 
         private readonly LinearPosition _position;
 
-        private readonly Lazy<IParsecStateStream<TToken>> _next;
+        private int Index => this._position.Column % MaxBufferSize;
 
-        public TToken Current { get; }
+        public TToken Current => this.buffer.Current[this.Index];
 
-        public bool HasValue { get; }
+        public bool HasValue => this.Index < this.buffer.Current.Length;
 
         public IPosition Position => this._position;
 
-        public IParsecStateStream<TToken> Next => this._next.Value;
+        public IParsecStateStream<TToken> Next => new EnumerableStream<TToken>((this.Index == MaxBufferSize - 1) ? this.buffer.Next : this.buffer, this._position.Next());
 
         public EnumerableStream(IEnumerable<TToken> source) : this(source.GetEnumerator())
         { }
 
-        public EnumerableStream(IEnumerator<TToken> enumerator) : this(enumerator, LinearPosition.Initial)
+        public EnumerableStream(IEnumerator<TToken> enumerator) : this(new Buffer(enumerator), LinearPosition.Initial)
         { }
 
-        private EnumerableStream(IEnumerator<TToken> enumerator, LinearPosition position)
+        private EnumerableStream(Buffer buffer, LinearPosition position)
         {
-            this.disposable = enumerator;
+            this.buffer = buffer;
             this._position = position;
-            try
-            {
-                this.HasValue = enumerator.MoveNext();
-                this.Current = (this.HasValue) ? enumerator.Current : default;
-            }
-            catch
-            {
-                this.HasValue = false;
-                this.Dispose();
-                throw;
-            }
-            finally
-            {
-                this._next = new Lazy<IParsecStateStream<TToken>>(() => new EnumerableStream<TToken>(enumerator, position.Next()), false);
-            }
         }
 
         public void Dispose()
-            => this.disposable.Dispose();
+            => this.buffer.Dispose();
 
         public bool Equals(IParsecState<TToken> other)
-            => ReferenceEquals(this, other);
+            => other is EnumerableStream<TToken> state && this.buffer == state.buffer && this._position == state._position;
+
+        public sealed override bool Equals(object obj)
+            => obj is EnumerableStream<TToken> state && this.buffer == state.buffer && this._position == state._position;
+
+        public sealed override int GetHashCode()
+            => this.buffer.GetHashCode() ^ this._position.GetHashCode();
 
         public sealed override string ToString()
             => (this.HasValue)
