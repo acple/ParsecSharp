4a476cafaebd05d6c3965c4940351efba618918a
acple <silentsphere110@gmail.com>
2019-06-06 11:03:33 +0900

    add standard Bytes operators

---
+++ ParsecSharp/Parser/Bytes.Prim.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using static ParsecSharp.Parser;
+
+namespace ParsecSharp
+{
+    public static partial class Bytes
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Parser<byte, byte> Any()
+            => Any<byte>();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Parser<byte, Unit> EndOfInput()
+            => EndOfInput<byte>();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Parser<byte, IEnumerable<byte>> Take(int count)
+            => Take<byte>(count);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Parser<byte, IEnumerable<byte>> TakeWhile(Func<byte, bool> predicate)
+            => TakeWhile<byte>(predicate);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Parser<byte, IEnumerable<byte>> TakeWhile1(Func<byte, bool> predicate)
+            => TakeWhile1<byte>(predicate);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Parser<byte, Unit> Skip(int count)
+            => Skip<byte>(count);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Parser<byte, Unit> SkipWhile(Func<byte, bool> predicate)
+            => SkipWhile<byte>(predicate);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Parser<byte, Unit> SkipWhile1(Func<byte, bool> predicate)
+            => SkipWhile1<byte>(predicate);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Parser<byte, byte> Satisfy(Func<byte, bool> predicate)
+            => Satisfy<byte>(predicate);
+    }
+}
