6ebbb8873b62f26aebb3619ac16ef3c061c13970
acple <silentsphere110@gmail.com>
2019-10-08 13:32:22 +0900

    add Map for Result

--- ParsecSharp/Core/Result/Success.cs
+++ ParsecSharp/Core/Result/Success.cs
@@ -30,6 +30,9 @@ namespace ParsecSharp
         public sealed override TResult CaseOf<TResult>(Func<Fail<TToken, T>, TResult> fail, Func<Success<TToken, T>, TResult> success)
             => success(this);
 
+        public sealed override Result<TToken, TResult> Map<TResult>(Func<T, TResult> function)
+            => new Success<TToken, TResult>(function(this.Value), this.Rest);
+
         public override string ToString()
             => this.Value?.ToString() ?? string.Empty;
     }
