4953a1ec2dcf3d18e62f3df68bad92da3524dc47
acple <silentsphere110@gmail.com>
2019-10-21 11:56:25 +0900

    add result validations for StackOverflowTest and adjust counts

--- UnitTest.ParsecSharp/StackOverflowTest.cs
+++ UnitTest.ParsecSharp/StackOverflowTest.cs
@@ -16,8 +16,9 @@ namespace UnitTest.ParsecSharp
         public void SimpleRecursionStackOverflowTest()
         {
             // 単純な再帰ループ
+            const int count = 1_000_000;
             var parser = SkipMany(Any<int>());
-            var source = new int[1000000];
+            var source = new int[count];
 
             parser.Parse(source);
         }
@@ -26,27 +27,33 @@ namespace UnitTest.ParsecSharp
         public void ValueTypesStackOverflowTest()
         {
             // トークンが値型の場合
+            const int count = 100_000;
             var parser = Many(Any<(int, int, int)>());
-            var source = Enumerable.Range(0, 1000000).Select(x => (x, x, x));
+            var source = Enumerable.Range(0, count).Select(x => (x, x, x));
 
-            parser.Parse(source);
+            parser.Parse(source).CaseOf(
+                fail => Assert.Fail(fail.ToString()),
+                success => success.Value.Count().Is(count));
         }
 
         [TestMethod]
         public void ReferenceTypesStackOverflowTest()
         {
             // トークンが参照型の場合
+            const int count = 100_000;
             var parser = Many(Any<Tuple<int, int, int>>());
-            var source = Enumerable.Range(0, 1000000).Select(x => Tuple.Create(x, x, x));
+            var source = Enumerable.Range(0, count).Select(x => Tuple.Create(x, x, x));
 
-            parser.Parse(source);
+            parser.Parse(source).CaseOf(
+                fail => Assert.Fail(fail.ToString()),
+                success => success.Value.Count().Is(count));
         }
 
         [TestMethod]
         public void RecursiveDataStructuresStackOverflowTest()
         {
             // 極端に深い構造を辿る場合
-            const int depth = 10000;
+            const int depth = 10_000;
             var source = Enumerable.Repeat('[', depth).Concat(Enumerable.Repeat(']', depth)).ToArray();
             var parser = Fix<char, int>(self => self.Or(Pure(1234)).Between(Char('['), Char(']'))).End();
 
