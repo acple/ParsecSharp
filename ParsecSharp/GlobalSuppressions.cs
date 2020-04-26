dababde0a8b99a4c01cc9ed4a232e1b5f82e25bc
acple <silentsphere110@gmail.com>
2020-04-26 14:07:25 +0900

    remove a suppression no longer needed

--- ParsecSharp/GlobalSuppressions.cs
+++ ParsecSharp/GlobalSuppressions.cs
@@ -1,4 +1,3 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0071:Simplify interpolation", Justification = "IDE0071 considerably decreases the performance of string construction")]
-[assembly: SuppressMessage("Style", "IDE0071WithoutSuggestion")]
