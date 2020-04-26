4bec7a99ae88f7d8a9d98c92628abdf7bf32f934
acple <silentsphere110@gmail.com>
2020-04-27 00:28:36 +0900

    add ExpressionParser example

---
+++ ParsecSharp.Examples/ExpressionParser.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using ParsecSharp;
+using static ParsecSharp.Parser;
+using static ParsecSharp.Text;
+
+namespace ParsecSharp.Examples
+{
+    public interface INumber<TNumber>
+        where TNumber : INumber<TNumber>
+    {
+        TNumber Add(TNumber value);
+
+        TNumber Sub(TNumber value);
+
+        TNumber Mul(TNumber value);
+
+        TNumber Div(TNumber value);
+    }
+
+    public class ExpressionParser<TNumber>
+        where TNumber : INumber<TNumber>
+    {
+        private static readonly Parser<char, Func<TNumber, TNumber, TNumber>> AddSub =
+            Choice(
+                Char('+').Between(Spaces()).Map(_ => (Func<TNumber, TNumber, TNumber>)((x, y) => x.Add(y))),
+                Char('-').Between(Spaces()).Map(_ => (Func<TNumber, TNumber, TNumber>)((x, y) => x.Sub(y))));
+
+        private static readonly Parser<char, Func<TNumber, TNumber, TNumber>> MulDiv =
+            Choice(
+                Char('*').Between(Spaces()).Map(_ => (Func<TNumber, TNumber, TNumber>)((x, y) => x.Mul(y))),
+                Char('/').Between(Spaces()).Map(_ => (Func<TNumber, TNumber, TNumber>)((x, y) => x.Div(y))));
+
+        private readonly Parser<char, TNumber> expr;
+
+        private readonly Parser<char, TNumber> term;
+
+        private readonly Parser<char, TNumber> factor;
+
+        public ExpressionParser(Parser<char, TNumber> number)
+        {
+            this.expr = Delay(() => this.term).ChainLeft(AddSub);
+            this.term = Delay(() => this.factor).ChainLeft(MulDiv);
+            this.factor = number | this.expr.Between(Char('(').Between(Spaces()), Char(')').Between(Spaces()));
+        }
+
+        public Result<char, TNumber> Parse(string source)
+            => this.expr.Parse(source);
+    }
+
+    public class Integer : INumber<Integer>
+    {
+        private static readonly Parser<char, Integer> number =
+            Many1(DecDigit()).ToInt().Map(x => new Integer(x));
+
+        public static ExpressionParser<Integer> Parser { get; } =
+            new ExpressionParser<Integer>(number);
+
+        public int Value { get; }
+
+        public Integer(int value)
+        {
+            this.Value = value;
+        }
+
+        public Integer Add(Integer value)
+            => new Integer(this.Value + value.Value);
+
+        public Integer Sub(Integer value)
+            => new Integer(this.Value - value.Value);
+
+        public Integer Mul(Integer value)
+            => new Integer(this.Value * value.Value);
+
+        public Integer Div(Integer value)
+            => new Integer(this.Value / value.Value);
+    }
+
+    public class Double : INumber<Double>
+    {
+        private static readonly Parser<char, Double> number =
+            from integer in Many1(DecDigit())
+            from fraction in Optional(Char('.').Append(Many1(DecDigit())), ".0")
+            select new Double(double.Parse(new string(integer.Concat(fraction).ToArray())));
+
+        public static ExpressionParser<Double> Parser { get; } =
+            new ExpressionParser<Double>(number);
+
+        public double Value { get; }
+
+        public Double(double value)
+        {
+            this.Value = value;
+        }
+
+        public Double Add(Double value)
+            => new Double(this.Value + value.Value);
+
+        public Double Sub(Double value)
+            => new Double(this.Value - value.Value);
+
+        public Double Mul(Double value)
+            => new Double(this.Value * value.Value);
+
+        public Double Div(Double value)
+            => new Double(this.Value / value.Value);
+    }
+
+    public class IntegerExpression : INumber<IntegerExpression>
+    {
+        private static readonly Parser<char, IntegerExpression> number =
+            Many1(DecDigit()).ToInt().Map(x => new IntegerExpression(Expression.Constant(x)));
+
+        public static ExpressionParser<IntegerExpression> Parser { get; } =
+            new ExpressionParser<IntegerExpression>(number);
+
+        private readonly Expression _value;
+
+        public Expression<Func<int>> Lambda => Expression.Lambda<Func<int>>(this._value);
+
+        public IntegerExpression(Expression value)
+        {
+            this._value = value;
+        }
+
+        public IntegerExpression Add(IntegerExpression value)
+            => new IntegerExpression(Expression.Add(this._value, value._value));
+
+        public IntegerExpression Sub(IntegerExpression value)
+            => new IntegerExpression(Expression.Subtract(this._value, value._value));
+
+        public IntegerExpression Mul(IntegerExpression value)
+            => new IntegerExpression(Expression.Multiply(this._value, value._value));
+
+        public IntegerExpression Div(IntegerExpression value)
+            => new IntegerExpression(Expression.Divide(this._value, value._value));
+    }
+}
