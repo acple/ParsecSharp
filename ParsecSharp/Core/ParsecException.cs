9d6b266c1ab00c2b791a22c552bfbf43dbd8610d
acple <silentsphere110@gmail.com>
2021-09-21 17:57:42 +0900

    drop netstandard1.6 and netcoreapp2.1 supports by runtimes EOLed

--- ParsecSharp/Core/ParsecException.cs
+++ ParsecSharp/Core/ParsecException.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace ParsecSharp
 {
-    public partial class ParsecException : Exception
+    [Serializable]
+    public class ParsecException : Exception
     {
         public ParsecException(string message) : base(message)
         { }
 
         public ParsecException(string message, Exception innerException) : base(message, innerException)
+        { }
+
+        protected ParsecException(SerializationInfo info, StreamingContext context) : base(info, context)
         { }
     }
 }
