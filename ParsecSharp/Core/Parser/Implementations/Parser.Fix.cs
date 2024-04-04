9c34ef919c594281065c5e68b69386e118eb60c2
acple <silentsphere110@gmail.com>
2024-04-04 12:27:29 +0900

    add missing sealed

--- ParsecSharp/Core/Parser/Implementations/Parser.Fix.cs
+++ ParsecSharp/Core/Parser/Implementations/Parser.Fix.cs
@@ -17,7 +17,7 @@ namespace ParsecSharp.Internal.Parsers
 
     internal sealed class Fix<TToken, TParamater, T>(Func<Func<TParamater, Parser<TToken, T>>, TParamater, Parser<TToken, T>> function, TParamater parameter) : Parser<TToken, T>
     {
-        internal override Result<TToken, TResult> Run<TState, TResult>(TState state, Func<Result<TToken, T>, Result<TToken, TResult>> cont)
+        internal sealed override Result<TToken, TResult> Run<TState, TResult>(TState state, Func<Result<TToken, T>, Result<TToken, TResult>> cont)
             => function(parameter => new Fix<TToken, TParamater, T>(function, parameter), parameter).Run(state, cont);
     }
 }
