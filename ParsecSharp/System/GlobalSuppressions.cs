e8e1450eede79add90231a4e15431dff1d76ec1b
acple <silentsphere110@gmail.com>
2024-08-06 23:56:47 +0900

    regenerate/update .editorconfig

--- ParsecSharp/System/GlobalSuppressions.cs
+++
@@ -1,3 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE0071:Simplify interpolation", Justification = "IDE0071 considerably decreases the performance of string construction")]
