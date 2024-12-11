f376bb4655a0e5e5543a2d6685369eda4b1571bc
acple <silentsphere110@gmail.com>
2024-12-12 01:26:21 +0900

    reimplement Bytes

--- ParsecSharp/Parser/Bytes/Bytes.Primitives.cs
+++ ParsecSharp/Parser/Bytes/Bytes.Primitives.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
 
 namespace ParsecSharp
 {
@@ -9,51 +6,79 @@ namespace ParsecSharp
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IParser<byte, short> Int16()
-            => Take(sizeof(short)).Map(bytes => BitConverter.ToInt16([.. bytes], 0));
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IParser<byte, int> Int32()
-            => Take(sizeof(int)).Map(bytes => BitConverter.ToInt32([.. bytes], 0));
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IParser<byte, long> Int64()
-            => Take(sizeof(long)).Map(bytes => BitConverter.ToInt64([.. bytes], 0));
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IParser<byte, ushort> UInt16()
-            => Take(sizeof(ushort)).Map(bytes => BitConverter.ToUInt16([.. bytes], 0));
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IParser<byte, uint> UInt32()
-            => Take(sizeof(uint)).Map(bytes => BitConverter.ToUInt32([.. bytes], 0));
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IParser<byte, ulong> UInt64()
-            => Take(sizeof(ulong)).Map(bytes => BitConverter.ToUInt64([.. bytes], 0));
+            => Take(sizeof(short)).Map(BinaryConvert.ToInt16);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IParser<byte, short> Int16BigEndian()
-            => Take(sizeof(short)).Map(bytes => BitConverter.ToInt16(bytes.Reverse().ToArray(), 0));
+            => Take(sizeof(short)).Map(BinaryConvert.ToInt16BigEndian);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, int> Int32()
+            => Take(sizeof(int)).Map(BinaryConvert.ToInt32);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IParser<byte, int> Int32BigEndian()
-            => Take(sizeof(int)).Map(bytes => BitConverter.ToInt32(bytes.Reverse().ToArray(), 0));
+            => Take(sizeof(int)).Map(BinaryConvert.ToInt32BigEndian);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, long> Int64()
+            => Take(sizeof(long)).Map(BinaryConvert.ToInt64);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IParser<byte, long> Int64BigEndian()
-            => Take(sizeof(long)).Map(bytes => BitConverter.ToInt64(bytes.Reverse().ToArray(), 0));
+            => Take(sizeof(long)).Map(BinaryConvert.ToInt64BigEndian);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, ushort> UInt16()
+            => Take(sizeof(ushort)).Map(BinaryConvert.ToUInt16);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IParser<byte, ushort> UInt16BigEndian()
-            => Take(sizeof(ushort)).Map(bytes => BitConverter.ToUInt16(bytes.Reverse().ToArray(), 0));
+            => Take(sizeof(ushort)).Map(BinaryConvert.ToUInt16BigEndian);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, uint> UInt32()
+            => Take(sizeof(uint)).Map(BinaryConvert.ToUInt32);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IParser<byte, uint> UInt32BigEndian()
-            => Take(sizeof(uint)).Map(bytes => BitConverter.ToUInt32(bytes.Reverse().ToArray(), 0));
+            => Take(sizeof(uint)).Map(BinaryConvert.ToUInt32BigEndian);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, ulong> UInt64()
+            => Take(sizeof(ulong)).Map(BinaryConvert.ToUInt64);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IParser<byte, ulong> UInt64BigEndian()
-            => Take(sizeof(ulong)).Map(bytes => BitConverter.ToUInt64(bytes.Reverse().ToArray(), 0));
+            => Take(sizeof(ulong)).Map(BinaryConvert.ToUInt64BigEndian);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, bool> Boolean()
+            => Any().Map(BinaryConvert.ToBoolean);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, char> Char()
+            => Take(sizeof(char)).Map(BinaryConvert.ToChar);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, char> CharBigEndian()
+            => Take(sizeof(char)).Map(BinaryConvert.ToCharBigEndian);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, double> Double()
+            => Take(sizeof(double)).Map(BinaryConvert.ToDouble);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, double> DoubleBigEndian()
+            => Take(sizeof(double)).Map(BinaryConvert.ToDoubleBigEndian);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, float> Single()
+            => Take(sizeof(float)).Map(BinaryConvert.ToSingle);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, float> SingleBigEndian()
+            => Take(sizeof(float)).Map(BinaryConvert.ToSingleBigEndian);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IParser<byte, sbyte> SByte()
@@ -61,6 +86,10 @@ namespace ParsecSharp
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static IParser<byte, string> Utf8String(int count)
-            => Take(count).Map(bytes => Encoding.UTF8.GetString([.. bytes]));
+            => Take(count).Map(BinaryConvert.ToUtf8String);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IParser<byte, string> Utf8StringBigEndian(int count)
+            => Take(count).Map(BinaryConvert.ToUtf8StringBigEndian);
     }
 }
