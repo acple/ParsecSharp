5c40f12b57b9b262550e875210361729d4a57bbb
acple <silentsphere110@gmail.com>
2025-12-08 16:34:09 +0900

    update .editorconfig

--- UnitTest.ParsecSharp/Program.cs
+++ UnitTest.ParsecSharp/Program.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0047:Remove unnecessary parentheses")]
+[assembly: SuppressMessage("Style", "SYSLIB1045:Use 'GeneratedRegexAttribute' to generate the regular expression implementation at compile-time.")]
 
 namespace UnitTest.ParsecSharp;
 
