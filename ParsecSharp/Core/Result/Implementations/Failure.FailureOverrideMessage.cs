412fa2877648c99f84703e5a79a757257244709b
acple <silentsphere110@gmail.com>
2025-12-12 22:47:49 +0900

    fix generic definition

--- ParsecSharp/Core/Result/Implementations/Failure.FailureOverrideMessage.cs
+++ ParsecSharp/Core/Result/Implementations/Failure.FailureOverrideMessage.cs
@@ -1,13 +1,13 @@
 namespace ParsecSharp.Internal.Results;
 
-internal sealed class FailureOverrideMessage<TToken, T>(IFailure<TToken, T> failure, string message) : Failure<TToken, T>
+internal sealed class FailureOverrideMessage<TToken, TInternal, T>(IFailure<TToken, TInternal> failure, string message) : Failure<TToken, T>
 {
     public sealed override IParsecState<TToken> State => failure.State;
 
     public sealed override string Message => message;
 
     public sealed override IFailure<TToken, TResult> Convert<TResult>()
-        => new FailureOverrideMessage<TToken, TResult>(failure.Convert<TResult>(), message);
+        => new FailureOverrideMessage<TToken, TInternal, TResult>(failure, message);
 
     public sealed override ISuspendedResult<TToken, T> Suspend()
         => SuspendedResult.Create(this, failure.Suspend().Rest);
--- ParsecSharp/Parser/Parser/Implementations/ModifyResult.OverrideMessage.cs
+++ ParsecSharp/Parser/Parser/Implementations/ModifyResult.OverrideMessage.cs
@@ -6,7 +6,7 @@ namespace ParsecSharp.Internal.Parsers;
 internal sealed class OverrideMessage<TToken, T>(IParser<TToken, T> parser, string message) : ModifyResult<TToken, T, T>(parser)
 {
     protected sealed override IResult<TToken, T> Fail<TState>(TState state, IFailure<TToken, T> failure)
-        => new FailureOverrideMessage<TToken, T>(failure, message);
+        => new FailureOverrideMessage<TToken, T, T>(failure, message);
 
     protected sealed override IResult<TToken, T> Succeed<TState>(TState state, ISuccess<TToken, T> success)
         => success;
@@ -15,7 +15,7 @@ internal sealed class OverrideMessage<TToken, T>(IParser<TToken, T> parser, stri
 internal sealed class OverrideMessageDelayed<TToken, T>(IParser<TToken, T> parser, Func<IFailure<TToken, T>, string> message) : ModifyResult<TToken, T, T>(parser)
 {
     protected sealed override IResult<TToken, T> Fail<TState>(TState state, IFailure<TToken, T> failure)
-        => new FailureOverrideMessage<TToken, T>(failure, message(failure));
+        => new FailureOverrideMessage<TToken, T, T>(failure, message(failure));
 
     protected sealed override IResult<TToken, T> Succeed<TState>(TState state, ISuccess<TToken, T> success)
         => success;
